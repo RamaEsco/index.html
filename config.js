67d44ce5b72533c55ff124201347180c06dc26b4
tatiana elizabeth tolaba <tolabatatiana300@gmail.com>
2025-12-03 19:25:13 -0300

    Eliminar config.js del repositorio público

--- config.js
+++
@@ -1,8 +0,0 @@
-// Configuración de Mercado Pago
-const MERCADO_PAGO_CONFIG = {
-    PUBLIC_KEY: 'APP_USR-46052b0d-a5fe-4a81-8944-fd7da23bbe3c',
-    ACCESS_TOKEN: 'APP_USR-6423900814501378-120220-42b13207cf585370f1cff8253b28eaac-1373171976',
-    // Otras configuraciones pueden ir aquí
-};
-
-// Asegúrate de que este archivo esté en .gitignore
